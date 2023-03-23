35916fb71ff7c6f27112570bf97839846b29433f
gstewar7 <gstewar7@calstatela.edu>
2023-03-23 03:40:21 -0700

    Removed console logging the added data

--- history.js
+++ history.js
@@ -20,8 +20,6 @@ const writeFunc = async (input) => {
       if (err)
         console.log(err);
       else {
-        console.log("File written successfully\n");
-        console.log("The written has the following contents:");
         console.log(fs.readFileSync("./history.json", "utf8"));
         console.log("File written successfully."); 
       }
