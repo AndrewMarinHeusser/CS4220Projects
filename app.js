0b14272af8171db85541ae4008a2f465adb41a40
Andrew Marin Heusser <heusser.andrew@gmail.com>
2023-03-23 23:47:14 -0700

    Final Pull Request (#2)
    * Code done before repository was created
    
    Pretty much everything up before adding prompts
    
    * Added prompts
    
    * Fixed entry search function
    
    * Added history
    
    Still needs a count of results.... and maybe minimizing the data but otherwise idk
    
    * Removed console logging the added data
    
    * Removed console logging the history file for real this time
    
    * Made history look nicer
    
    * Updated history.json
    
    Fixed the key names to fit the requirements of the assignment
    
    * Update history.json
    
    ---------
    
    Co-authored-by: gstewar7 <gstewar7@calstatela.edu>

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const prompts = require('prompts');
 const api = require('./api.js');
+const hist = require('./history.js');
+const date = new Date();
+const dateObj = `${date.getMonth()}/${date.getDate()}, ${date.getHours()}:${date.getMinutes()}`;
+
 
 const searchCategory = async (args) => {
     console.log("args", args);
@@ -8,14 +12,18 @@ const searchCategory = async (args) => {
     
     //returns a list of indeces selected by the user from the result of the categorical search
     const selections = await selectEntry(result);
-
+    const selectionList = [];
     //loop through the selected objects
     for(const i in selections['entries'])
     {
         //use the index from selectEntry to find information about the selected entries from the result of 
         //the categorical search
-        const currentEntry = result['data'][selections['entries'][i]]
-
+        const currentEntry = result['data'][selections['entries'][i]];
+        const historyEntry = {
+            "name" : currentEntry['name'],
+            "Category" : currentEntry['category'],
+            "id" : currentEntry['id']
+        }
         //loops through all the keys of each object to print out in a cleaner fashion than a blob of json 
         for(const [key, value] of Object.entries(currentEntry))
         {
@@ -23,15 +31,26 @@ const searchCategory = async (args) => {
             if(key !== 'image')
                 console.log(`${key}: ${value}`);
         }
+        selectionList.push(historyEntry);
         console.log('\n')
     }
     
+    const historyArr = {"resultCount" : selections['entries'].length,  "search": selectionList, "Search Date": dateObj};
+    hist.writeFunc(historyArr);
 }
+
 
 //search for a specific entry, no prompt required on this one
 const searchEntry = async (args) => {
     console.log("args", args);
     const result  = await api.getEntry(args.entry);
+    const historyEntry = [{
+        "name" : result['data']['name'],
+        "Category" : result['data']['category'],
+        "id" : result['data']['id']
+    }];
+    const historyArr = {"resultCount" : 1,"search": historyEntry,"Search Date": dateObj};
+    hist.writeFunc(historyArr);
     console.log(result);
     return result;
 }
@@ -79,3 +98,4 @@ module.exports = {
     selectEntry,
     displayCategory
 };
+
